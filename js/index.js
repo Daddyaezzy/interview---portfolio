5a8f98662a10c97ce95d83474d1cd193cb839d58
daddyaezzy <pulumbu11@gmail.com>
2023-08-07 17:34:57 +0100

    commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function toggleMenu() {
+  // Get the hamburger icon and menu elements
+  var hamburgerIcon = document.querySelector(".hamburger i");
+
+  var menu = document.querySelector(".nav__list");
+
+  console.log(menu);
+
+  // Toggle the 'active' class on the menu element
+  menu.classList.toggle("active");
+
+  // Toggle the 'fa-bars' and 'fa-times' classes on the hamburger icon
+  if (hamburgerIcon.classList.contains("fa-bars")) {
+    hamburgerIcon.classList.remove("fa-bars");
+    hamburgerIcon.classList.add("fa-times");
+  } else {
+    hamburgerIcon.classList.remove("fa-times");
+    hamburgerIcon.classList.add("fa-bars");
+  }
+}
